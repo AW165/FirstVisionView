4f97a8486b23edeb472128d043928f3452581e6e
Cxixi <c3136459784@163.com>
2026-04-22 08:52:55 +0800

    20260422保存

--- FristVisionView/ParameterUILibary/ParameterModel/binaryzationModel.cs
+++ FristVisionView/ParameterUILibary/ParameterModel/binaryzationModel.cs
@@ -15,20 +15,43 @@ namespace FirstVisionView.ParameterUILibary.ParameterModel
     {
         public BinaryzationModel()
         {
+            this.Title = "二值化";
+
             // 因为继承了 BaseParamenter，所以可以直接调用 ParameterList
+            this.ParameterList.Add(new ComboboxParameterItem
+            {
+                Name = "图像来源",
+                Options = new ObservableCollection<string> { },
+
+
+            });
+            this.ParameterList.Add(new ComboboxParameterItem
+            {
+                Name = "阈值方式",
+                Options = new ObservableCollection<string> { "单阈值", "双阈值" },
+                SelectedValue = "单阈值"
+            });
             this.ParameterList.Add(new SliderParameterItem { 
                 Name = "阈值" ,
                 Max = 255,
                 Min = 0,
                 Step = 1,
             });
+
             this.ParameterList.Add(new ComboboxParameterItem
             {
-                Name = "阈值方式",
-                Options = new ObservableCollection<string> { "单阈值","双阈值" },
-                SelectedValue = "单阈值"
+                Name = "ROI区域",
+                Options = new ObservableCollection<string> { },
+
+
             });
-            this.Title = "二值化";
+            this.ParameterList.Add(new ComboboxParameterItem
+            {
+                Name = "位置修正参考",
+                Options = new ObservableCollection<string> { },
+
+
+            });
         }
     }
    
